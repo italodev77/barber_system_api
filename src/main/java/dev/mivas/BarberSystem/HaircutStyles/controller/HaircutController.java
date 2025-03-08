8d5a319d4e17de594dde02fd91a35af0628f455e
devitalo <italo_ariel_dev77@outlook.com>
2025-03-08 01:29:03 -0300

    feat: implementado controller para haircuts

--- src/main/java/dev/mivas/BarberSystem/HaircutStyles/controller/HaircutController.java
+++ src/main/java/dev/mivas/BarberSystem/HaircutStyles/controller/HaircutController.java
@@ -1,4 +1,28 @@
 package dev.mivas.BarberSystem.HaircutStyles.controller;
 
+import dev.mivas.BarberSystem.HaircutStyles.entity.HaircutModel;
+import dev.mivas.BarberSystem.HaircutStyles.request.HaircutRequest;
+import dev.mivas.BarberSystem.HaircutStyles.response.HaircutResponse;
+import dev.mivas.BarberSystem.HaircutStyles.services.HaircutServices;
+import dev.mivas.BarberSystem.Mapper.HaircutMapper;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/barber/haircuts")
+@RequiredArgsConstructor
 public class HaircutController {
+    private final HaircutServices haircutServices;
+
+    @PostMapping("/register")
+    public ResponseEntity<HaircutResponse> Register(@Valid @RequestBody HaircutRequest haircutRequest){
+        HaircutModel savedHaircut = haircutServices.createHaircut(HaircutMapper.toHaircut(haircutRequest));
+        return ResponseEntity.status(HttpStatus.CREATED).body((HaircutMapper.toHaircutResponse(savedHaircut)));
+    }
 }
